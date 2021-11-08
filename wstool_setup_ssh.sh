1fa9a442a44598468a7a7aef26377e8cf8c802de
TooSchoolForCool <zeyuz@outlook.com>
2021-11-08 10:53:03 +0800

    add map_proc wstool dep

--- wstool_setup_ssh.sh
+++ wstool_setup_ssh.sh
@@ -3,5 +3,5 @@ wstool init src/dependency
 wstool merge -t src/dependency src/Interactive-Scene-Reconstruction/mapping/orb_slam_2_ros/orb_slam2_ros_ssh.rosinstall
 wstool merge -t src/dependency src/Interactive-Scene-Reconstruction/mapping/perception_ros/perception_ros_ssh.rosinstall
 wstool merge -t src/dependency src/Interactive-Scene-Reconstruction/mapping/voxblox-plusplus/voxblox-plusplus_ssh.rosinstall
-# wstool merge -t src/dependency src/Interactive-Scene-Reconstruction/cad_replacement/map_proc/map_proc_ssh.rosinstall
+wstool merge -t src/dependency src/Interactive-Scene-Reconstruction/cad_replacement/map_proc/map_proc_ssh.rosinstall
 wstool update -t src/dependency
